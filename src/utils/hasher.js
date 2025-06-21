252fd591651fd1a6bb20e7b1bd9d04fe4dbf4afa
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-06-21 14:53:19 +0530

    fix(hasher): use dynamic hash algorithm from configuration

--- src/utils/hasher.js
+++ src/utils/hasher.js
@@ -1,6 +1,6 @@
-import { hashEncoder } from '../config/hashConfig.js';
+import { hashAlgorithm, hashEncoder } from '../config/hashConfig.js';
 import crypto from 'crypto';
 const getHash = (str) => {
-  return crypto.createHash('sha256').update(str).digest(hashEncoder);
+  return crypto.createHash(hashAlgorithm).update(str).digest(hashEncoder);
 };
 export default getHash;
